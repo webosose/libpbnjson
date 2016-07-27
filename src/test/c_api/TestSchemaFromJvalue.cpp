ef0b931b9d2f6ebcb9f7e51f1744155d1c38281f
Nikolay Orliuk <nikolay.orliuk@lge.com>
2016-07-27 14:13:10 +0300

    Use constructed jvalue in TestSchemaFromJvalue
    :Release Notes:
    
    :Detailed Notes:
    Extend test coverage by TestSchemaFromJvalue:
    - add checking for natively constructed jvalue alongside with parsed
      from string jvalue.
    - additionally check behavior of jvalue_validate()
    - additionally check behavior of jvalue_validate_apply()
    
    :Testing Performed:
    Unit-tests under desktop
    
    :QA Notes:
    
    :Issues Addressed:
    [PLAT-24269] Garbage after application of JSON schema constructed from
                 JValue with default values inside.
    
    Change-Id: Ia3b642bcad04364693e9492dc217f901803ed1fd
    Reviewed-on: https://gpro.lgsvl.com/149724
    Reviewed-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    Tested-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    Reviewed-by: Anatolii Sakhnik <anatolii.sakhnik@lge.com>

--- src/test/c_api/TestSchemaFromJvalue.cpp
+++ src/test/c_api/TestSchemaFromJvalue.cpp
@@ -21,6 +21,7 @@
 #include <gtest/gtest.h>
 
 #include "pbnjson.h"
+#include "pbnjson.hpp"
 #include "../../pbnjson_c/validation/validator.h"
 
 #include "TestUtils.hpp"
@@ -41,6 +42,12 @@ namespace {
 		);
 	};
 
+	auto jvalue_dup = [](jvalue_ref value) {
+		return mk_ptr(
+			jvalue_duplicate(value)
+		);
+	};
+
 	auto schema_jvalue_old = [](jvalue_ref value) {
 		return mk_ptr(
 			jschema_parse_jvalue(value, nullptr, "")
@@ -53,28 +60,43 @@ namespace {
 		);
 	};
 
-	typedef function< decltype(schema_jvalue(nullptr))(jvalue_ref) > schema_jvalue_t;
+	auto native_jval_repr = [](pbnjson::JValue &repr) {
+		return mk_ptr(jvalue_copy(repr.peekRaw()));
+	};
+
+	auto parse_jval_repr = [](pbnjson::JValue &repr) {
+		return jvalue_str(jvalue_tostring(repr.peekRaw(), jschema_all()));
+	};
+
+	typedef decltype(jvalue_str(nullptr)) jvalue_t;
+	typedef decltype(schema_str(nullptr)) schema_t;
+	typedef function< schema_t(jvalue_ref) > schema_jvalue_t;
+	typedef function< jvalue_t(pbnjson::JValue &) > repr_t;
 } // anonymous namespace
 
 using ::testing::TestWithParam;
 using ::testing::Values;
 using ::testing::Combine;
 
-class SchemaFromJvalue : public TestWithParam<tr1::tuple<const char *, const char *, schema_jvalue_t>>
+class SchemaFromJvalue : public TestWithParam<tr1::tuple<pbnjson::JValue, const char *, schema_jvalue_t, repr_t>>
 { };
 
 TEST_P(SchemaFromJvalue, Basic)
 {
 	auto param = GetParam();
-	static_assert( 3 == tr1::tuple_size<decltype(param)>::value, "param should be a tuple of 3 values" );
-	auto schema = get<0>(param);
+	static_assert( 4 == tr1::tuple_size<decltype(param)>::value, "param should be a tuple of 4 values" );
+	auto schema_repr = get<0>(param);
 	auto json = get<1>(param);
 	auto schema_jvalue_f = get<2>(param);
+	auto repr_f = get<3>(param);
+
+	const char *schema = jvalue_tostring(schema_repr.peekRaw(), jschema_all());
+	ASSERT_TRUE( !!schema );
 
 	auto schema_s = schema_str(schema);
 	ASSERT_TRUE( !!schema_s );
 
-	auto parsed_schema = jvalue_str(schema);
+	auto parsed_schema = repr_f(schema_repr);
 	ASSERT_TRUE( !!parsed_schema );
 
 	auto schema_j = schema_jvalue_f(parsed_schema.get());
@@ -82,39 +104,74 @@ TEST_P(SchemaFromJvalue, Basic)
 
 	auto value_s = jvalue_str(json, schema_s.get());
 	auto value_j = jvalue_str(json, schema_j.get());
+	auto value = jvalue_str(json);
+	auto value_apply_s = jvalue_dup(value.get()); // need independent value
+	auto value_apply_j = jvalue_dup(value.get());
 
-	// note that jerror() have same representation with null, so we'll check it
-	// separately
-	EXPECT_EQ( jis_valid(value_s.get()), jis_valid(value_j.get()) );
+	// note that jinvalid() have same representation with jnull(), so we'll
+	// check it separately
+	EXPECT_EQ( jis_valid(value_s.get()), jis_valid(value_j.get()) )
+		<< "There should be no difference in parsing with schema built from string and from jvalue should";
+	EXPECT_EQ( jis_valid(value_s.get()), jis_valid(value.get()) && jvalue_validate(value.get(), schema_s.get(), nullptr) )
+		<< "Result of validation against schema during parsing from text and after should give same results";
+	EXPECT_EQ( jis_valid(value_s.get()), jis_valid(value.get()) && jvalue_validate(value.get(), schema_j.get(), nullptr) )
+		<< "Validation against schema built from text during parsing shouldn't be different"
+		<< " from validation of jvalue against schema built from jvalue";
+	EXPECT_EQ( jis_valid(value_s.get()), jis_valid(value_apply_s.get()) && jvalue_validate_apply(value_apply_s.get(), schema_s.get(), nullptr) )
+		<< "Result of validation against schema during parsing from text and after should give same results";
+	EXPECT_EQ( jis_valid(value_s.get()), jis_valid(value_apply_j.get()) && jvalue_validate_apply(value_apply_j.get(), schema_j.get(), nullptr) )
+		<< "Validation against schema built from text during parsing shouldn't be different"
+		<< " from validation of jvalue against schema built from jvalue";
 
-	// we do a special comparison for null values
-	if (jis_null(value_s.get()) || jis_null(value_j.get()))
+	// Now let's compare values (if they are different from jinvalid())
+	EXPECT_TRUE( jvalue_equal(value_s.get(), value_j.get()) )
+		<< "Expecting " << jvalue_tostring(value_s.get(), jschema_all())
+		<< ", but got " << jvalue_tostring(value_j.get(), jschema_all());
+
+	if (jis_valid(value_s.get()))
 	{
-		EXPECT_EQ( jis_null(value_s.get()), jis_null(value_j.get()) );
-	}
-	else // fill for non-null values only
-	{
-		// we relay on the fact that different results have different string
-		// representations using strings gives nicier output
-		const char *str_s = jvalue_tostring(value_s.get(), jschema_all());
-		const char *str_j = jvalue_tostring(value_j.get(), jschema_all());
+		// Note that value_apply_* will be a valid jvalue as long as we get a
+		// valid json on input regardless of validation against schema.
+		EXPECT_TRUE( jvalue_equal(value_s.get(), value_apply_s.get()) )
+			<< "Expecting " << jvalue_tostring(value_s.get(), jschema_all())
+			<< ", but got " << jvalue_tostring(value_apply_s.get(), jschema_all());
 
-		EXPECT_STREQ( str_s, str_j );
+		EXPECT_STREQ( jvalue_tostring(value_s.get(), jschema_all()),
+		              jvalue_tostring(value_apply_s.get(), jschema_all()) );
+
+		EXPECT_TRUE( jvalue_equal(value_s.get(), value_apply_j.get()) )
+			<< "Expecting " << jvalue_tostring(value_s.get(), jschema_all())
+			<< ", but got " << jvalue_tostring(value_apply_j.get(), jschema_all());
+
+		EXPECT_STREQ( jvalue_tostring(value_s.get(), jschema_all()),
+		              jvalue_tostring(value_apply_j.get(), jschema_all()) );
 	}
 }
+
+using jval = pbnjson::JValue;
+using jobj = pbnjson::JObject;
+using jarr = pbnjson::JArray;
 
 // cartesian product of schemas vs json values
 INSTANTIATE_TEST_CASE_P(Samples, SchemaFromJvalue,
 	Combine(
 		Values(
-			"{}",
-			"{\"type\":\"integer\"}",
-			"{\"type\":\"array\", \"uniqueItems\":true}",
-			"{\"properties\":{\"foo\":{\"default\":false}}}",
-			"{\"properties\":{\"foo\":{\"type\":\"integer\"}}}",
-			"{\"oneOf\":[{\"type\":\"string\"},{\"type\":\"integer\"}]}",
-			"{\"definitions\":{\"foo\":{\"$ref\":\"#/definitions/bar\"},\"bar\":{\"type\":\"integer\"}},\"oneOf\":[{\"$ref\":\"#/definitions/foo\"}]}",
-			"{\"additionalProperties\":{\"type\":\"integer\"}}"
+			jobj {},
+			jobj {{"type", "integer"}},
+			jobj {{"type", "array"}, {"uniqueItems", true}},
+			jobj {{"properties", {{"foo", {{"default", false}}}}}},
+			jobj {{"properties", {{"foo", {{"type", "integer"}}}}}},
+			jobj {{"type", "object"}, {"properties", {{"foo", {{"type", "integer"}, {"default", 3}}}}}},
+			jobj {{"type", "object"}, {"properties", {{"foo", {{"type", "number"}, {"default", 0.5}}}}}},
+			jobj {{"oneOf", jarr {{{"type", "string"}}, {{"type", "integer"}}}}},
+			jobj {
+				{"definitions", {
+					{"foo", {{ "$ref", "#/definitions/bar" }}},
+					{"bar", {{ "type", "integer" }}}
+				}},
+				{"oneOf", jarr {{{"$ref", "#/definitions/foo" }}}}
+			},
+			jobj {{"additionalProperties", {{"type", "integer"}}}}
 		),
 		Values(
 			"1",
@@ -133,6 +190,10 @@ INSTANTIATE_TEST_CASE_P(Samples, SchemaFromJvalue,
 		Values(
 			schema_jvalue_t(schema_jvalue_old),
 			schema_jvalue_t(schema_jvalue)
+		),
+		Values(
+			repr_t(native_jval_repr), // use jvalue as-is
+			repr_t(parse_jval_repr) // obtain jvalue through stringify->parse
 		)
 	)
 );
