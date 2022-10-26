d36c6dbf88887bfd12b42d42a4493d2db9ea9a0a
yogish.s <yogish.s@lge.com>
2022-10-26 09:08:41 +0530

    Fix Coverity Issue
    :Release Notes:
    Incorrect deallocator used. Used correct deallocator,
    g_slice_free1() to deallocate the memory.
    
    :Detailed Notes:
    Depending on the difference between the correct and
    incorrect allocator, there may be a resource leak
    or memory corruption.
    
    :Testing Performed:
    Locally Tested.
    
    :QA Notes:
    
    :Issues Addressed:
    [WRO-14707] 9330723 Incorrect deallocator used (libpbnjson)
    
    Change-Id: Idf807dc78eef18bb1a9a98ddd433b73ebab68326

--- src/pbnjson_c/jobject.c
+++ src/pbnjson_c/jobject.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2021 LG Electronics, Inc.
+// Copyright (c) 2009-2022 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -498,7 +498,7 @@ jvalue_ref jobject_create ()
 	                                           _ObjKeyValDestroy, _ObjKeyValDestroy);
 	if (!new_obj->m_members)
 	{
-		free(new_obj);
+		g_slice_free1 (sizeof(jobject),new_obj);
 		return NULL;
 	}
 	TRACE_REF("created", new_obj);
